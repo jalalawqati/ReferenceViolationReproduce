3331fefa7fbdde977ba319a8418c977679dbfe55
jalalawqati <jalalawqati@icloud.com>
2026-09-15 20:43:39 +0200

    Initial Commit

---
+++ ReferenceViolationReproduce/ReferenceViolationReproduceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReferenceViolationReproduceApp.swift
+//  ReferenceViolationReproduce
+//
+//  Created by Jalal on 15.09.26.
+//
+
+import SwiftUI
+
+@main
+struct ReferenceViolationReproduceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
